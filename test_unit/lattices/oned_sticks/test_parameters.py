af52d3513e92cd911f1c4db8fbaf8e9ecc0c3388
Andres <andrumen1@yahoo.com>
2023-11-09 19:37:54 -0600

    Added preliminary tests.

--- test_unit/lattices/oned_sticks/test_parameters.py
+++ test_unit/lattices/oned_sticks/test_parameters.py
@@ -4,7 +4,5 @@ from pathlib import Path
 import src.lattices.oned_sticks.parameters as lparams
 
 
-
-FPATH = f"{Path(__file__).parent.resolve()}"
-
-lparams.create_configuration(FPATH)
+configuration = lparams.get()
+lparams.validate(configuration)
