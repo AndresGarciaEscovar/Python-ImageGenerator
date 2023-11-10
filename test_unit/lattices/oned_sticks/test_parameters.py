9d61dd039a428586d2bff484bc0e82389f2b648f
andres <andrumen1@yahoo.com>
2023-11-10 01:43:46 -0600

    Runs the whole thing. Must test.

--- test_unit/lattices/oned_sticks/test_parameters.py
+++ test_unit/lattices/oned_sticks/test_parameters.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 import src.lattices.oned_sticks.parameters as lparams
 
-
+print("\nTesting parameters.py")
 configuration = lparams.get()
 lparams.validate(configuration)
+print("\nDone testing parameters.py")
