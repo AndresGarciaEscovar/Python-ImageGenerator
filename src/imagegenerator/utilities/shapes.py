d20de13f88152290b2df362dd069e3e86649d720
Andres Garcia Escovar <andrumen1@yahoo.com>
2026-03-09 17:18:57 -0500

    Updated to get a single tick.

--- src/imagegenerator/utilities/shapes.py
+++ src/imagegenerator/utilities/shapes.py
@@ -4,37 +4,17 @@
 
 
 # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-# Functions - Auxiliary
+# Imports
 # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
 
-def _validate_coordinate(coordinate: tuple) -> None:
-    """
-        Validates that the given coordinate is a real number, i.e., an integer
-        or a floating point number.
+# User.
+import imagegenerator.validate.shapes as vshapes
 
-        :param coordinate: A collection of numbers that represent an
-         N-dimensional coordinate.
 
-        :raise TypeError: If the coordinates are not real numbers, and the
-         coordinate itself is NOT a tuple.
-    """
-    # Must be a tuple.
-    if not (isinstance(coordinate, tuple) and len(coordinate) == 2):
-        flag: bool = isinstance(coordinate, tuple)
-        msg: str = "" if not flag else f"; current length {len(coordinate)}"
-
-        raise TypeError(
-            f"The coordinate must be a 2-tuple of real numbers; current type "
-            f"{type(coordinate).__name__}{msg}."
-        )
-
-    # A tuple of real number.
-    if not all(isinstance(x, (float, int)) for x in coordinate):
-        raise TypeError(
-            "There are variables in the coordinate that are not real numbers; "
-            "make sure to only give real numbers."
-        )
+# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+# Functions - Auxiliary
+# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
 
 # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
@@ -51,9 +31,49 @@ def get_coordinate(coordinate: tuple) -> str:
         :return: A string representing the coordinate.
     """
     # Validate the coordinate.
-    _validate_coordinate(coordinate)
+    vshapes.validate_coordinate(coordinate)
 
     return f"({', '.join(f'{float(x):+.4f}' for x in coordinate)})"
+
+
+def get_tick(
+    base: float = 0.5,
+    vertical: float = 1.5,
+    position: tuple = (0, 0),
+    offset: tuple = (0, 0),
+) -> str:
+    """
+        Gets the code for a tick.
+
+        :param base: The base of the tick; must be a positive real number
+         greater than zero.
+
+        :param vertical: The length of the vertical part of the tick.
+
+        :param position: A tuple that represents the position of the middle of
+         point of the base along the base.
+
+        :param offset: The horizontal and vertical offset of the lower part of
+         the tick with respect to the horizontal middle of the base (along the
+         x-axis), and above the base (along the y-axis). Negative numbers
+         represent left/right and top/bottom.
+
+        :return: A string with the TikZ code for a simple tick.
+    """
+    # Get the coordinates.
+    positions_base: tuple = (
+        (-base / 2.0 + position[0], position[1]),
+        (+base / 2.0 + position[0], position[1]),
+    )
+    positions_vertical: tuple = (
+        (position[0] + offset[0], position[1] + offset[1]),
+        (position[0] + offset[0], position[1] + offset[1] + vertical),
+    )
+
+    positions_base = tuple(get_coordinate(x) for x in positions_base)
+    positions_vertical = tuple(get_coordinate(x) for x in positions_vertical)
+
+    return f"{' - '.join(positions_base)}\n{' - '.join(positions_vertical)}"
 
 
 # #############################################################################
@@ -68,7 +88,7 @@ def run() -> None:
     numbers: tuple = 9, 0
     coordinate: str = get_coordinate(numbers)
 
-    print(coordinate)
+    print(get_tick())
 
 
 # -----------------------------------------------------------------------------
--- src/imagegenerator/validate/shapes.py
+++ src/imagegenerator/validate/shapes.py
@@ -3,19 +3,38 @@
     functions.
 """
 
-
-# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-# Imports
-# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-
-# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-# Functions - Auxiliary
-# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-
-
 # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 # Functions
 # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
 
-def validate_parameters() -> None:
+def validate_coordinate(coordinate: tuple) -> None:
+    """
+        Validates that the given coordinate is a real number, i.e., an integer
+        or a floating point number.
+
+        :param coordinate: A collection of numbers that represent an
+         N-dimensional coordinate.
+
+        :raise TypeError: If the coordinates are not real numbers, and the
+         coordinate itself is NOT a tuple.
+    """
+    # Must be a tuple.
+    if not (isinstance(coordinate, tuple) and len(coordinate) == 2):
+        flag: bool = isinstance(coordinate, tuple)
+        msg: str = "" if not flag else f"; current length {len(coordinate)}"
+
+        raise TypeError(
+            f"The coordinate must be a 2-tuple of real numbers; current type "
+            f"{type(coordinate).__name__}{msg}."
+        )
+
+    # A tuple of real number.
+    if not all(isinstance(x, (float, int)) for x in coordinate):
+        raise TypeError(
+            "There are variables in the coordinate that are not real numbers; "
+            "make sure to only give real numbers."
+        )
+
+
+def validate_tick
